dd2743a5b646474f770d52ada005b1c7ad57e052
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-21 13:27:36 -0400

    Creates location

--- Gemfile
+++ Gemfile
@@ -27,8 +27,7 @@ gem 'turbolinks', '~> 5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+gem 'geocoder'
 
 group :development, :test do
   gem 'pry-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921165716) do
+ActiveRecord::Schema.define(version: 20170921172351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20170921165716) do
   create_table "episodes_users", id: false, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "episode_id", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shows", force: :cascade do |t|
