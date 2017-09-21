74fffbe55bbb49c0169fbaad1d32f3c22956cad2
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-21 15:24:28 -0400

    Adds show and episode index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'shows#index'
+
+  resources :shows, only: [:index, :show] do
+    resources :episodes, only: [:index]
+  end
 end
