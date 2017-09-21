64c71d7fca39caa4af5d10cdb480cad6a18d7bb6
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-21 14:04:44 -0400

    Creates tracks

---
+++ app/models/track.rb
@@ -0,0 +1,3 @@
+class Track < ApplicationRecord
+  validates :title, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921180232) do
+ActiveRecord::Schema.define(version: 20170921180347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(version: 20170921180232) do
   create_table "shows_users", id: false, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "show_id", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
