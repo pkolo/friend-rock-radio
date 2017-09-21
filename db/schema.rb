bb252405c92df56ff0e3dd071c4e690d66a70b57
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-21 14:03:07 -0400

    Creates labels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921172926) do
+ActiveRecord::Schema.define(version: 20170921180232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 20170921172926) do
   create_table "episodes_users", id: false, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "episode_id", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "locations", force: :cascade do |t|
