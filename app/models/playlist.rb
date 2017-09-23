edc8aa5efad0df6a6a9fc891eb8ae865da2dc5f5
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-23 17:25:04 -0400

    Fixes track building

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,8 @@
 class Playlist < ApplicationRecord
   belongs_to :episode
-  belongs_to :track
+  belongs_to :track, optional: true
+
+  validates :track, presence: true
 
   accepts_nested_attributes_for :track
 end
