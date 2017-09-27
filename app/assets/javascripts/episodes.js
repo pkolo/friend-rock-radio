87277eea585de13452322da7e5be1f44d5d15092
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-27 16:22:49 -0400

    Adds close btn to modal

--- app/assets/javascripts/episodes.js
+++ app/assets/javascripts/episodes.js
@@ -1,15 +1,18 @@
 $(document).on('turbolinks:load', function() {
   $('#playlists').on('cocoon:after-insert', function(e, newPlaylist) {
-    // $('.track-form').hide()
+
     $('.create-playlist-btn').on('click', function(e) {
       e.preventDefault();
-
     })
+
+    $('#playlists').on('click', '.close', function(e) {
+      $(this).parents('.modal')[0].style.display = 'none';
+    })
+
     $(newPlaylist).find('.selectize').selectize({
       create: function(input, callback) {
-        $('.track-form').show();
+        $('.modal').show();
         $('.track-title-input').val(input);
-
       }
     });
   })
