a59e898cf27724bd667e5d58ce8b975123189436
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-28 11:52:34 -0400

    Clear form after successful submit

--- app/assets/javascripts/episodes.js
+++ app/assets/javascripts/episodes.js
@@ -1,30 +1,44 @@
+var selectizeCallback = null;
+
 $(document).on('turbolinks:load', function() {
+  // After clicking 'Add playlist item'
   $('#playlists').on('cocoon:after-insert', function(e, newPlaylist) {
 
     $('.create-playlist-btn').on('click', function(e) {
       e.preventDefault();
     })
 
+
+    $('#new_track').on('submit', function(e) {
+      e.preventDefault();
+      $.ajax({
+        method: "POST",
+        url: $(this).attr('action'),
+        data: $(this).serialize(),
+        success: function(response) {
+          console.log(response)
+          selectizeCallback({value: response.id, text: response.title});
+          selectizeCallback = null;
+          $('.modal').remove();
+        }
+      })
+    });
+
     $('#playlists').on('click', '.close', function(e) {
       $(this).parents('.modal')[0].style.display = 'none';
+      if (selectizeCallback != null) {
+        selectizeCallback();
+        selectizeCallback = null;
+      }
     })
 
     $(newPlaylist).find('.selectize').selectize({
       create: function(input, callback) {
-        $('.modal').show();
-        $('.track-title-input').val(input);
+        selectizeCallback = callback;
 
-        $('#new_track').on('submit', function(e) {
-          e.preventDefault();
-          $.ajax({
-            method: "POST",
-            url: $(this).attr('action'),
-            data: $(this).serialize(),
-            success: function(response) {
-              console.log(response)
-            }
-          })
-        })
+        $('.modal').show();
+        $('#new_track').trigger('reset');
+        $('.track-title-input').val(input);
       }
     });
   })
