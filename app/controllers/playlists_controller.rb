18588c8a86463711cf3bfe346aaa20637e32dac9
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-29 12:00:05 -0400

    Create action for playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,31 +1,17 @@
 class PlaylistsController < ApplicationController
   def create
-    binding.pry
     @episode = Episode.find(params[:episode_id])
     @playlist = Playlist.new(playlist_params)
     @playlist.episode = @episode
+    if @playlist.save
+      render json: @playlist, status: :created
+    else
+      render json: { errors: @playlist.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
     def playlist_params
-      params.require(:playlist).permit(:time_marker, track_attributes: [
-                      :id,
-                      :title,
-                      :year,
-                      :_destroy,
-                      bands_attributes: [
-                        :id,
-                        :name
-                      ],
-                      genres_attributes: [
-                        :id,
-                        :name
-                      ],
-                      labels_attributes: [
-                        :id,
-                        :name
-                      ]
-                   ]
-                  )
+      params.require(:playlist).permit(:time_marker, :track_id)
     end
 end
