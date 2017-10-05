22e7c3534929e0b23f5dc0757fc80955d50eae5e
Patrick Kolodgy <pkolodgy@gmail.com>
2017-10-05 07:56:17 -0400

    Cleans up episodes controller

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,75 +4,9 @@ class EpisodesController < ApplicationController
     @episodes = @show.episodes.all
   end
 
-  def new
-    @show = Show.find(params[:show_id])
-    @episode = Episode.new
-  end
-
-  def create
-    @show = Show.find(params[:show_id])
-    @episode = Episode.new(episode_params)
-    @episode.show = @show
-    if @episode.save
-      redirect_to edit_show_episode_path @show, @episode
-    else
-      @errors = @episode.errors.full_messages
-      render 'new'
-    end
-  end
-
   def show
     @show = Show.find(params[:show_id])
     @episode = Episode.find(params[:id])
     @playlists = @episode.playlists.play_order
   end
-
-  def edit
-    @show = Show.find(params[:show_id])
-    @episode = Episode.find(params[:id])
-    @playlists = @episode.playlists.order(:time_marker)
-  end
-
-  def update
-    @show = Show.find(params[:show_id])
-    @episode = Episode.find(params[:id])
-    if @episode.update(episode_params)
-      redirect_to show_episode_path @show, @episode
-    else
-      @errors = @episode.errors.full_messages
-      render 'edit'
-    end
-  end
-
-  private
-    def episode_params
-      params.require(:episode)
-            .permit(:title,
-                    :description,
-                    :published,
-                    playlists_attributes: [
-                      :time_marker, track_attributes: [
-                                    :id,
-                                    :title,
-                                    :year,
-                                    :_destroy,
-                                    bands_attributes: [
-                                      :id,
-                                      :name,
-                                      :_destroy
-                                    ],
-                                    genres_attributes: [
-                                      :id,
-                                      :name,
-                                      :_destroy
-                                    ],
-                                    labels_attributes: [
-                                      :id,
-                                      :name,
-                                      :_destroy
-                                    ]
-                                 ]
-                              ]
-                            )
-    end
 end
